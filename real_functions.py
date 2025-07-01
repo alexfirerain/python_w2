8c4912f9299c91c2636c8e1c5b9261c412759e2e
alexfirerain <alexfirerain@yandex.ru>
2025-07-01 16:12:56 +0300

    functions go

--- real_functions.py
+++ real_functions.py
@@ -11,4 +11,37 @@ def square(num):
     temp = num ** 2
     return temp
 
+
 t = square(5)
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        return "чёт"
+    else:
+        return "нечет"
+
+
+# однако это не лучший варик
+def even_odd(num):
+    if num % 2 == 0:
+        return "чёт"
+    return "нечет"
+
+
+def even_odd(num):
+    return "чёт" if num % 2 == 0 else "нечет"
+
+
+def print_string(s=None):
+    if s is None:
+        return  # пример выпрыга
+    print(s)
+
+"""
+Д/З
+выводить число словами, т.е. 356 → "триста пятьдесят шесть"
+до максимум трёхзначного.
+написать функцию.
+
+"""
